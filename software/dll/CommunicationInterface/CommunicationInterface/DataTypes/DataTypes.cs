c3940982229fc104d0412a9c7a45b7cd6ce491cd
faber.dirk <faber.dirk@51c5fc4c-a7ff-11dd-a77a-2d921c902ce3>
2008-11-21 13:39:13 +0000

    dll gedöns

---
+++ software/dll/CommunicationInterface/CommunicationInterface/DataTypes/DataTypes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommunicationAPI.DataTypes
+{
+    public struct Session
+    {
+
+    }
+
+    public struct User
+    {
+        private string userName;
+        private 
+    }
+
+    public struct Lamp
+    {
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public Lamp(int id)
+        {
+            this.id = id;
+        }
+    }
+
+    public struct Region
+    {
+    }
+
+    public struct Product
+    {
+    }
+
+    public struct Sign
+    {
+    }
+
+    public struct Trace
+    {
+    }
+
+    public struct Advertisement
+    {
+    }
+}
