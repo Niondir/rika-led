e2d5ed803186bd3349fd5c9360ab53add923eb8e
quick_silver@gmx.de <quick_silver@gmx.de@51c5fc4c-a7ff-11dd-a77a-2d921c902ce3>
2008-11-30 14:08:16 +0000

    missing files

---
+++ trunk/software/dll/CommunicationAPI/CommunicationAPI/AccessFlags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommunicationAPI
+{
+    [Flags]
+    public enum AccessFlags : int
+    {
+        None = 0,
+        Guest = 1,
+        Authenticated = 2
+    }
+}
--- trunk/software/dll/CommunicationAPI/CommunicationAPI/DataTypes/DataTypes.cs
+++ trunk/software/dll/CommunicationAPI/CommunicationAPI/DataTypes/DataTypes.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography;
 
 namespace CommunicationAPI.DataTypes
 {
+ 
     public struct Session
     {
         private int id;
