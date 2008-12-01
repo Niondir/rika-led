c53e89f6851883d10ebd744f52d612b854c03041
quick_silver@gmx.de <quick_silver@gmx.de@51c5fc4c-a7ff-11dd-a77a-2d921c902ce3>
2008-12-01 15:21:54 +0000

    sql files, keine ahnung wie man die automatisch in das erstellungsverzeichniss bekommt , jetzt müssen die erstmal immer überall geändert werden

--- software/server/StoreServer/Data/DataManager.cs
+++ software/server/StoreServer/Data/DataManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Data.Odbc;
+using System.IO;
 
 namespace StoreServer.Data
 {
@@ -72,13 +73,136 @@ namespace StoreServer.Data
             // Finde die info tabelle
             // lese version (wenn tabelle fehlt -> version = 0)
             // Führe alle SQL Files von gefundener Version bis Programm.Version aus
+            bool found = false;
             while (reader.Read())
             {
                 string table = reader.GetString(0);
-                Debug.WriteLine("Table: " + table);
-
+                if (table == "led_informations")
+                {
+                    found = true;
+                    break;
+                }
             }
+            reader.Close();
+
+            Version version = new Version(0, 0, 0, 0);
+
+            if (!found)
+            {
+                Debug.WriteLine("Information table not found");
+
+                OdbcTransaction transaction = connection.BeginTransaction();
+
+                try {
+                    command.Transaction = transaction;
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE `rika`.`led_informations` (
+                                          `key` VARCHAR(255) NOT NULL,
+                                          `value` VARCHAR(255) NOT NULL,
+                                          PRIMARY KEY (`key`)
+                                          );";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"INSERT INTO `rika`.`led_informations` (`key`, `value`) VALUES('version','0.0.0.0');";
+                    command.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                    Debug.WriteLine("Information table created");
+                }
+                catch (Exception ex) {
+                    transaction.Rollback();
+                    Debug.WriteLine("Created informations table, Error: " + ex.Message);
+                }
+            }
+
+            // Get version
+            command.CommandText = "SELECT `value` FROM `rika`.`led_informations` WHERE `key` = 'version'";
+            reader = command.ExecuteReader();
+
+           
+            while (reader.Read())
+            {
+                string v = reader.GetString(0);
+                try
+                {
+                    version = new Version(v);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            BuildDatabase(version);
+
+        }
+
+        /// <summary>
+        /// Erstelle die Datenbank aus den SQL Dateien
+        /// </summary>
+        /// <param name="version"></param>
+        private void BuildDatabase(Version version)
+        {
+            string dir = Program.BaseDirectory + "/sql";
+            if (!Directory.Exists(dir))
+                return;
+
+
+            SortedDictionary<string, Version> files = new SortedDictionary<string, Version>();
             
+
+            foreach (string f in Directory.GetFiles(dir))
+            {
+                string file = Path.GetFileName(f);
+                Version fileversion = new Version(file.Split('_')[0]);
+                if (fileversion > version)
+                {
+                    files.Add(f, fileversion);
+                }
+            }
+
+            OdbcCommand command = connection.CreateCommand();
+
+            foreach (string f in files.Keys)
+            {
+                Debug.WriteLine("Executing SQL file: " + f);
+
+                FileStream fs = File.Open(f, FileMode.Open);
+                TextReader reader = new StreamReader(fs);
+
+                string content = reader.ReadToEnd();
+                fs.Close();
+
+                string[] querys = content.Split(new Char[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+
+                command.Transaction = connection.BeginTransaction();
+                try
+                {
+                    foreach (string q in querys)
+                    {
+                        if (string.IsNullOrEmpty(q.Trim())) continue;
+                        command.CommandText = q;
+                        command.Prepare();
+                        command.ExecuteNonQuery();
+                    }
+
+                    OdbcCommandBuilder cb = new OdbcCommandBuilder();
+                    command.CommandText = "UPDATE `rika`.`led_informations` SET `value` = ? WHERE `key` = 'version';";
+                    //command.Parameters.AddWithValue("?version?", files[f].ToString());
+                    command.Parameters.Add("version", OdbcType.VarChar).Value = files[f].ToString();
+                    command.ExecuteNonQuery();
+
+                    command.Transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    // Try to rollback the executed SQL file
+                    command.Transaction.Rollback();
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
 
         private void CreateTables()
